a381c3c4477e107e714c4bd187ede8e62e8d9d8f
Peter Bauer <p.bauer@htl-leonding.ac.at>
2018-04-07 23:39:49 +0200

    Created project

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -2,7 +2,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
-    public static Matcher getMatcher(String dateString) {
+    public static Matcher getMatcher(String dateStrings) {
         return null;
     }
 }
