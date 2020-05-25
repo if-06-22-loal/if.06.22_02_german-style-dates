407900fafb37d25da1c275eea60f6f5f45c93a32
Peter Bauer <p.bauer@htl-leonding.ac.at>
2020-05-25 08:36:30 +0200

    Migrated to junit 5

--- src/test/java/GermanStyleDateCheckerTests.java
+++ src/test/java/GermanStyleDateCheckerTests.java
@@ -1,21 +1,8 @@
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
 import java.util.regex.Matcher;
-
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GermanStyleDateCheckerTests {
-    /* Write a regular expression that matches German written dates.
-    They are in the form dd.mm.yyyy, where the separator ”.” can
-    also be ”-” or a blank. The leading digit d or m can be omitted.
-    Valid years are between 1900 and 2099. But the leading two yy
-    can also be omitted.
-
-    Take care that the unit tests have to pass in the order as given
-    in this file here. Tests passing after one failed test will not
-    count.
-     */
-
     @Test
     public void testSimpleValidDate() {
         checkRegex("23.03.1967");
